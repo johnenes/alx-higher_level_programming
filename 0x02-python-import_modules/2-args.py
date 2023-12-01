a52c67a61f20b102cb7278fb9108bd8203036ec5
johnenes <johneneojo1@gmail.com>
2023-12-01 11:34:12 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    i = len(sys.argv) - 1
+    print('Number of Argument: ', len(sys.argv) - 1, 'Argument')
+    print('Argument List: ', str(sys.argv))
