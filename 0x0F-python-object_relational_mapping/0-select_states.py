58e8736b85cb8091f6cbc114b1d6675068365559
johnenes <johneneojo1@gmail.com>
2024-03-28 13:02:09 -0700

    execution command

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,45 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+def list_states(username, password, database):
+    db = None
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost",
+                             user=username,
+                             passwd=password,
+                             db=database,
+                             port=3306)
+        
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute SQL query to fetch states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all rows
+        states = cursor.fetchall()
+
+        # Print results
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
+
--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+import mysql.connector
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            port=3306
+        )
+        cur = db.cursor()
+        cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                    cities INNER JOIN states ON states.id=cities.state_id""")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+
