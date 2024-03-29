87585878b286fd8014390d2c9261cede9d0f8542
johnenes <johneneojo1@gmail.com>
2024-03-29 15:21:19 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Script that fetch https://alx-intranet.hbtn.io/status
+    using urllib
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    html = response.read().decode('utf-8')
+print("Body response:")
+print("\t- type:" ,type(html))
+print("\t- content:", html)
