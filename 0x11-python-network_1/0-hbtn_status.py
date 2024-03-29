7d28567188203f5f380904ff36250d2832ea4717
johnenes <johneneojo1@gmail.com>
2024-03-29 15:23:42 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     html = response.read().decode('utf-8')
 print("Body response:")
-print("\t- type:" ,type(html))
-print("\t- content:", html)
+print("\t- type: {} " ,type(html))
+print("\t- content: {} ", html)
